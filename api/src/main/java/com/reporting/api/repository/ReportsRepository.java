c4580061a9e81c58037a79da8206edad70ebca8c
isyepr <32873316+isyepr@users.noreply.github.com>
2019-05-14 07:18:57 +0700

    Update ReportsRepository.java

--- api/src/main/java/com/reporting/api/repository/ReportsRepository.java
+++ api/src/main/java/com/reporting/api/repository/ReportsRepository.java
@@ -1,6 +1,5 @@
 package com.reporting.api.repository;
 
-import com.reporting.api.model.Details;
 import com.reporting.api.model.Reports;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
