75a155bdb0d1743ab388d3885747e9434cc4fe67
isyepr <32873316+isyepr@users.noreply.github.com>
2019-05-14 07:18:49 +0700

    Update ReportsController.java

--- api/src/main/java/com/reporting/api/controller/ReportsController.java
+++ api/src/main/java/com/reporting/api/controller/ReportsController.java
@@ -3,7 +3,6 @@ package com.reporting.api.controller;
 import com.reporting.api.exception.ResourceNotFoundException;
 import com.reporting.api.model.Reports;
 import com.reporting.api.repository.ReportsRepository;
-import com.reporting.api.repository.DetailsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,38 +21,38 @@ public class ReportsController {
     public List<Reports> getAllReports() {
         return reportsRepository.findAll();
     }
+    
     // Create a new Report
-    @PostMapping("/reports")
-    public Reports createReport(@Valid @RequestBody Reports report) {
-        return reportsRepository.save(report);
-    }
+//    @PostMapping("/reports")
+//    public Reports createReport(@Valid @RequestBody Reports report) {
+//        return reportsRepository.save(report);
+//    }
+    
     // Get 1 Report
-    @GetMapping("/reports/{id_laporan}")
-    public Reports getReportById(@PathVariable(value = "id_laporan") Long reportId) {
+    @GetMapping("/reports/{id_report}")
+    public Reports getReportById(@PathVariable(value = "id_report") Long reportId) {
         return reportsRepository.findById(reportId)
-                .orElseThrow(() -> new ResourceNotFoundException("laporan", "id_laporan", reportId));
+                .orElseThrow(() -> new ResourceNotFoundException("reports", "id_report", reportId));
     }
+    
+    // Get All Reports By Role
+    @GetMapping("/reports/{role}")
+    public Reports getRoleById(@PathVariable(value = "role") Long role) {
+        return reportsRepository.findById(role)
+                .orElseThrow(() -> new ResourceNotFoundException("reports", "role", role));
+    }
+    
     // Update a report
-    @PutMapping("/reports/{id_laporan}")
-    public Reports updateReport(@PathVariable(value = "id_laporan") Long reportId,
+    @PutMapping("/reports/{id_report}")
+    public Reports updateReport(@PathVariable(value = "id_report") Long reportId,
                                 @Valid @RequestBody Reports reportDetails) {
 
         Reports reports = reportsRepository.findById(reportId)
-                .orElseThrow(() -> new ResourceNotFoundException("lapora", "id_laporan", reportId));
+                .orElseThrow(() -> new ResourceNotFoundException("reports", "id_report", reportId));
 
-        reports.setTgl_laporan(reportDetails.getTgl_laporan());
+        reports.setTanggal_laporan(reportDetails.getTanggal_laporan());
 
         Reports updatedReport = reportsRepository.save(reports);
         return updatedReport;
-    }
-    // Delete a report
-    @DeleteMapping("/reports/{id_laporan}")
-    public ResponseEntity<?> deleteReport(@PathVariable(value = "id_laporan") Long reportId) {
-        Reports reports = reportsRepository.findById(reportId)
-                .orElseThrow(() -> new ResourceNotFoundException("laporan", "id_laporan", reportId));
-
-        reportsRepository.delete(reports);
-
-        return ResponseEntity.ok().build();
     }
 }
